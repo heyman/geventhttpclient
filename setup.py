7fa9b8348cc48560061e57d8f7c6281d01135119
Jonatan Heyman <jonatan@heyman.info>
2017-12-23 15:03:28 +0100

    Reverted changes to setup.py

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ if sys.hexversion < 0x02070900:
     ]
 
 
-setup(name='geventhttpclient-wheels',
+setup(name='geventhttpclient',
       version = '1.3.1dev',
-      description = 'Python wheels for geventhttpclient',
+      description = 'http client library for gevent',
       long_description = DESC,
       url="http://github.com/gwik/geventhttpclient",
       author="Antonin Amand",
